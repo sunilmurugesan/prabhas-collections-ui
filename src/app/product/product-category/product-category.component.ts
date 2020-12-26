06684f2747c407947a883d67a372afdffaf5f1d1
Sunil Murugesan <Sunil.Murugesan@santander.co.uk>
2020-12-26 18:18:50 +0000

    Plumping in home component and install required dependencies.

---
+++ src/app/product/product-category/product-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pc-product-category',
+  templateUrl: './product-category.component.html',
+  styleUrls: ['./product-category.component.css']
+})
+export class ProductCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
